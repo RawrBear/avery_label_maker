0011442fd306427f78afd2c438da923c079b33d2
Neil <nr.brand@yahoo.com>
2023-04-03 14:11:23 -0400

    Added a timer to the save function o give Python time to write the file. Also added a global counter to generte sheet name every time the function is executed

--- labelmaker.py
+++ labelmaker.py
@@ -1,13 +1,20 @@
 from docxtpl import DocxTemplate
 import pandas as pd
 
+import time as time
+
 # Opens the template file
 doc = DocxTemplate("template.docx")
 
 # Sets the main context to be an empty dictionary
 main_context = {}
 
-# Person class
+counter = 0
+
+
+# TODO Test with bigger dataset
+# TODO Change dataset to fake data before making git repo
+# TODO Make git repo
 
 
 class Person:
@@ -35,13 +42,16 @@ class Person:
 
 # Save the file
     def saveFile():
-        counter = 0
+        global counter
         counter = counter + 1
+        print("Counter is:", counter)
         doc.save("sheet"+str(counter)+".docx")
+        print("SHEET SAVED")
+        main_context.clear()
 
 
 # get dataframe from excel file
-df = pd.read_excel("birthday_list.xlsx", usecols=[
+df = pd.read_excel("testdata.xlsx", usecols=[
                    "NAME", "ADDRESS", "CITY", "STATE", "ZIP"])
 
 # Get length of dataframe
@@ -51,18 +61,31 @@ datasetLen = len(df.index)
 for index, row in df.iterrows():
     print("Index is: ", index)
 
+    # Check if we hit every 20 in the dataset
+    if index + 1 in range(0, datasetLen, 20):
+        print("In Range: ", index + 1)
+
+        # # Send data to the class
+        person = Person(row["NAME"], row["ADDRESS"],
+                        row["CITY"], row["STATE"], row["ZIP"], index)
+        doc.render(main_context)
+        Person.saveFile()
+        #  Print the done message
+        print("DONE WITH PAGE")
+        time.sleep(5)
+        continue
+
 # Send data to the class
     person = Person(row["NAME"], row["ADDRESS"],
                     row["CITY"], row["STATE"], row["ZIP"], index)
 
     # Check if we are at the end of the dataset
     if index + 1 >= datasetLen:
+        # Send data to the class
+        person = Person(row["NAME"], row["ADDRESS"],
+                        row["CITY"], row["STATE"], row["ZIP"], index)
         doc.render(main_context)
         Person.saveFile()
 #  Print the done message
         print("DONE")
         break
-
-# TODO Test with bigger dataset
-# TODO Change dataset to fake data before making git repo
-# TODO Make git repo
